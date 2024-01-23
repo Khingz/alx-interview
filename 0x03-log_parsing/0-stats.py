a335981545d78549242b017aeabe0930027ab79a
khingz <kingsley995mr@gmail.com>
2024-01-23 06:01:27 -0800

    forst push

---
+++ 0x03-log_parsing/0-stats.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""Comment"""
+import sys
+
+
+if __name__ == "__main__":
+    size = 0
+    code = {200: 0, 301: 0, 400: 0, 401: 0, 403: 0, 404: 0, 405: 0, 500: 0}
+    for index, line in enumerate(sys.stdin, start=1):
+        line_size = len(line.encode("utf-8"))
+        size += line_size
+        items = line.strip().split()
+        key = int(items[7])
+        code[key] += 1
+        if index % 10 == 0:
+            print("File size: {}".format(size))
+            for k, v in code.items():
+                print("{}: {}".format(k, v))
