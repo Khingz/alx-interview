41f0b97fbe5d6a6acc27f440ee0763db60906c70
Kingsley Akpan <54294050+Khingz@users.noreply.github.com>
2024-03-05 14:21:50 +0000

    check failing

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -4,21 +4,25 @@
 
 
 def island_perimeter(grid):
-    """ island perimeter function
-    """
     perimeter = 0
-    for i in range(len(grid)):
-        for j in range(len(grid[i])):
+    rows = len(grid)
+    if rows == 0:
+        return perimeter
+    cols = len(grid[0])
+    
+    for i in range(rows):
+        for j in range(cols):
             if grid[i][j] == 1:
-                try:
-                    if i > 0 and grid[i - 1][j] == 0:
-                        perimeter += 1
-                    if i < len(grid) - 1 and grid[i + 1][j] == 0:
-                        perimeter += 1
-                    if j > 0 and grid[i][j - 1] == 0:
-                        perimeter += 1
-                    if j < len(grid[i]) - 1 and grid[i][j + 1] == 0:
-                        perimeter += 1
-                except IndexError:
+                # Check top neighbor
+                if i == 0 or grid[i - 1][j] == 0:
                     perimeter += 1
-    return (perimeter)
+                # Check bottom neighbor
+                if i == rows - 1 or grid[i + 1][j] == 0:
+                    perimeter += 1
+                # Check left neighbor
+                if j == 0 or grid[i][j - 1] == 0:
+                    perimeter += 1
+                # Check right neighbor
+                if j == cols - 1 or grid[i][j + 1] == 0:
+                    perimeter += 1
+    return perimeter
