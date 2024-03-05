036fbec3ff7a4f909e7e96ca1ba9ef01785efa8c
Kingsley Akpan <54294050+Khingz@users.noreply.github.com>
2024-03-05 13:36:47 +0000

    perimeter solution

---
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+""" Permieter island
+"""
+
+
+def island_perimeter(grid):
+    """ island perimeter function
+    """
+    perimeter = 0
+    for i in range(len(grid)):
+        for j in range(len(grid[i])):
+            if grid[i][j] == 1:
+                try:
+                    if grid[i - 1][j] == 0:
+                        perimeter += 1
+                    if grid[i + 1][j] == 0:
+                        perimeter += 1
+                    if grid[i][j - 1] == 0:
+                        perimeter += 1
+                    if grid[i][j + 1] == 0:
+                        perimeter += 1
+                except Exceptions:
+                    pass
+    return (perimeter)
